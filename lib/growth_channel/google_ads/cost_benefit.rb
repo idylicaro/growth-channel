75f7b7bfab3d703f34af5c93dd119cd57893c41c
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-06 00:49:03 -0300

    feat(cost_benefit): ensure sort by investment cost with cost benefit

--- lib/growth_channel/google_ads/cost_benefit.rb
+++ lib/growth_channel/google_ads/cost_benefit.rb
@@ -11,18 +11,23 @@ module GoogleAds
     def per_video(sort_increasing: false, filter_tag: '')
       group_videos = []
       group_main_video_info(filter_tag: filter_tag).each do |video|
-        generate_score(group_videos, video)
+        id_video_principal = video[:id_video_principal]
+        group_videos.push({id_video_principal: id_video_principal, cost_benefit: calculate_benefit_cost(video)})
       end
       sort_increasing ? group_videos.sort_by { |element| element[:cost_benefit] }.reverse : group_videos
     end
 
-    private
-
-    def generate_score(group_videos, video)
-      id_video_principal = video[:id_video_principal]
-      cost_benefit = calculate_benefit_cost(video)
-      group_videos.push({id_video_principal: id_video_principal, cost_benefit: cost_benefit})
+    def with_cost_per_video(sort_increasing: false, filter_tag: '')
+      group_videos = []
+      group_main_video_info(filter_tag: filter_tag).each do |video|
+        id_video_principal = video[:id_video_principal]
+        cost = video[:cost].floor(2)
+        group_videos.push({id_video_principal: id_video_principal, cost: cost, cost_benefit: calculate_benefit_cost(video)})
+      end
+      sort_increasing ? group_videos.sort_by { |element| element[:cost] }.reverse : group_videos
     end
+
+    private
 
     def calculate_benefit_cost(video)
       cost_benefit = video[:views] + (video[:watched_25] * 2) + (video[:watched_50] * 4) + (video[:watched_75] * 5) + (video[:watched_100] * 3)
